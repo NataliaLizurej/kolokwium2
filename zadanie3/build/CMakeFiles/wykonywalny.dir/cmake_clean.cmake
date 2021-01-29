91f3004e3880d0a28b3659a901496488223a0b45
Natalia Lizurej <74413186+NataliaLizurej@users.noreply.github.com>
2021-01-29 14:20:55 +0100

    Add files via upload

---
+++ zadanie3/build/CMakeFiles/wykonywalny.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wykonywalny.dir/address.cpp.o"
+  "CMakeFiles/wykonywalny.dir/employee.cpp.o"
+  "CMakeFiles/wykonywalny.dir/main.cpp.o"
+  "wykonywalny"
+  "wykonywalny.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wykonywalny.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
